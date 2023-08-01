eb2e9f680fd0690f7514a28b89b473703cf1bba7
minseok1109 <park485201@naver.com>
2023-08-01 20:16:06 +0900

    fix: 글이 아무것도 없는 상황일 때 수정

--- app/page.tsx
+++ app/page.tsx
@@ -8,11 +8,15 @@ export default async function Home() {
   const products = await getProductsData();
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      {products?.map((product: Product) => (
-        <Link href={`products/${product.post_id}`} key={product.post_id}>
-          <Card product={product} />
-        </Link>
-      ))}
+      {products ? (
+        products?.map((product: Product) => (
+          <Link href={`products/${product.post_id}`} key={product.post_id}>
+            <Card product={product} />
+          </Link>
+        ))
+      ) : (
+        <div>글이 없습니다.</div>
+      )}
     </main>
   );
 }
