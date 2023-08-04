baf97d86add68fbe6144dd2f722a12155d753e07
minseok1109 <park485201@naver.com>
2023-08-04 14:33:51 +0900

    feat: 실시간 입찰자 수 수정

--- app/components/InputModal.tsx
+++ app/components/InputModal.tsx
@@ -2,79 +2,78 @@
 
 import socket from "@/utils/socket";
 import {
-  BID,
-  INPUT_BID,
-  INPUT_COST,
-  CLOSE,
-  LOWER_THAN_PRICE,
-  ONLY_NUMBER,
+    BID,
+    INPUT_BID,
+    INPUT_COST,
+    CLOSE,
+    LOWER_THAN_PRICE,
+    ONLY_NUMBER,
 } from "../constant";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 
 declare global {
-  interface Window {
-    inputPriceModal: HTMLDialogElement;
-  }
+    interface Window {
+        inputPriceModal: HTMLDialogElement;
+    }
 }
 
 const InputModal = ({
-  productPrice,
-  productId,
-  userId,
-}: {
-  productPrice: number;
-  productId: number;
-  userId: number;
+                        productPrice,
+                        productId,
+                        userId,
+                    }: {
+    productPrice: number;
+    productId: number;
+    userId: number;
 }) => {
-  const [bidPrice, setBidPrice] = useState("");
+    const [bidPrice, setBidPrice] = useState("");
 
-  const postPrice = (price: string) => {
-    //TODO: 서버에 입찰가격 전송해야 함 (socket.io or rest api)
-    const numPrice = Number(price);
-    const numProductPrice = Number(productPrice);
+    const postPrice = (price: string) => {
+        const numPrice = Number(price);
+        const numProductPrice = Number(productPrice);
 
-    if (numPrice < numProductPrice) {
-      alert(LOWER_THAN_PRICE);
-    } else if (isNaN(numPrice)) {
-      alert(ONLY_NUMBER);
-    } else {
-      setBidPrice("");
-    }
-    socket.emit("sendPrice", {
-      user_id: userId,
-      post_id: productId,
-      price: Number(bidPrice),
-    });
-  };
+        if (numPrice < numProductPrice) {
+            alert(LOWER_THAN_PRICE);
+        } else if (isNaN(numPrice)) {
+            alert(ONLY_NUMBER);
+        } else {
+            setBidPrice("");
+        }
+        socket.emit("sendPrice", {
+            user_id: userId,
+            post_id: productId,
+            price: Number(bidPrice),
+        });
+    };
 
-  return (
-    <>
-      <button
-        className="btn btn-primary"
-        onClick={() => window.inputPriceModal.showModal()}
-      >
-        {BID}
-      </button>
-      <dialog id="inputPriceModal" className="modal">
-        <form method="dialog" className="modal-box">
-          <h3 className="mb-3 text-lg font-bold">{INPUT_BID}</h3>
-          <input
-            type="text"
-            value={bidPrice}
-            onChange={(e) => setBidPrice(e.target.value)}
-            placeholder={INPUT_COST}
-            className="w-full max-w-lg input input-bordered"
-          />
-          <div className="modal-action">
-            <button className="btn" onClick={() => postPrice(bidPrice)}>
-              {BID}
+    return (
+        <>
+            <button
+                className="btn btn-primary"
+                onClick={() => window.inputPriceModal.showModal()}
+            >
+                {BID}
             </button>
-            <button className="btn">{CLOSE}</button>
-          </div>
-        </form>
-      </dialog>
-    </>
-  );
+            <dialog id="inputPriceModal" className="modal">
+                <form method="dialog" className="modal-box">
+                    <h3 className="mb-3 text-lg font-bold">{INPUT_BID}</h3>
+                    <input
+                        type="text"
+                        value={bidPrice}
+                        onChange={(e) => setBidPrice(e.target.value)}
+                        placeholder={INPUT_COST}
+                        className="w-full max-w-lg input input-bordered"
+                    />
+                    <div className="modal-action">
+                        <button className="btn" onClick={() => postPrice(bidPrice)}>
+                            {BID}
+                        </button>
+                        <button className="btn">{CLOSE}</button>
+                    </div>
+                </form>
+            </dialog>
+        </>
+    );
 };
 
 export default InputModal;
--- app/components/UserCountChart.tsx
+++ app/components/UserCountChart.tsx
@@ -1,199 +1,189 @@
 "use client";
 
-import React, { useState, useEffect, useRef } from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
 } from "chart.js";
-import { Line } from "react-chartjs-2";
+import {Line} from "react-chartjs-2";
 import axios from "axios";
 import dayjs from "dayjs";
-import { URL } from "../constant";
+import {URL} from "../constant";
 
 interface BizPriceType {
-  biz_id: number;
-  biz_price: number;
-  createdAt: string;
-  post_id: number;
-  user_id: number;
+    biz_id: number;
+    biz_price: number;
+    createdAt: string;
+    post_id: number;
+    user_id: number;
 }
 
 interface ChartDataState {
-  labels: string[];
-  datasets: {
-    label: string;
-    yAxisID?: string;
-    data: number[];
-    borderColor: string;
-    backgroundColor: string;
-  }[];
+    labels: string[];
+    datasets: {
+        label: string;
+        yAxisID?: string;
+        data: number[];
+        borderColor: string;
+        backgroundColor: string;
+    }[];
 }
 
 ChartJS.register(
-  Tooltip,
-  Legend,
-  Title,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement
+    Tooltip,
+    Legend,
+    Title,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement
 );
 
 const options = {
-  responsive: false,
-  maintainAspectRatio: false,
-  animation: {
-    duration: 0,
-  },
-  scales: {
-    "y-right": {
-      position: "right",
-      ticks: {
-        stepSize: 1,
-      },
+    responsive: false,
+    maintainAspectRatio: false,
+    animation: {
+        duration: 0,
     },
-  },
-  plugins: {
-    legend: {
-      position: "top" as const,
+    scales: {
+        "y-right": {
+            position: "right",
+            ticks: {
+                stepSize: 1,
+            },
+        },
     },
-  },
+    plugins: {
+        legend: {
+            position: "top" as const,
+        },
+    },
 };
 
 //중복된 유저 카운트
 function countDuplicates(arr: string[]) {
-  const countMap: {
-    [key: string]: number;
-  } = {};
+    const countMap: {
+        [key: string]: number;
+    } = {};
 
-  for (const item of arr) {
-    countMap[item] = (countMap[item] || 0) + 1;
-  }
-
-  const duplicates: {
-    [key: string]: number;
-  } = {};
-  for (const key in countMap) {
-    if (countMap[key] > 1) {
-      duplicates[key] = countMap[key];
+    for (const item of arr) {
+        countMap[item] = (countMap[item] || 0) + 1;
     }
-  }
 
-  return duplicates;
+
+    return countMap;
 }
 
 function UserCountChart({
-  productId,
-  label,
-  lineColor,
-}: {
-  productId: number;
-  label: string;
-  lineColor: string;
+                            productId,
+                            label,
+                            lineColor,
+                        }: {
+    productId: number;
+    label: string;
+    lineColor: string;
 }) {
-  const chartRef = useRef<HTMLDivElement>(null);
-  const [data, setData] = useState<ChartDataState>({
-    labels: [],
-    datasets: [
-      {
-        label,
-        yAxisID: "y-right",
-        data: [],
-        borderColor: lineColor,
-        backgroundColor: lineColor.split(")").join(", 0.5)"),
-      },
-    ],
-  });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get(`${URL}/post/posts/biz/${productId}`);
-      const { data } = result;
-
-      //x축 라벨
-      const labels = data.map((item: BizPriceType) =>
-        dayjs(item.createdAt).format("MM-DD HH:mm")
-      );
-
-      //y축 데이터
-      const userCount = Object.values(countDuplicates(data));
-      console.log(countDuplicates(labels));
-
-      setData({
-        labels,
+    const chartRef = useRef<HTMLDivElement>(null);
+    const [data, setData] = useState<ChartDataState>({
+        labels: [],
         datasets: [
-          {
-            label,
-            yAxisID: "y-right",
-            data: userCount,
-            borderColor: lineColor,
-            backgroundColor: lineColor.split(")").join(", 0.5)"),
-          },
-        ],
-      });
-    };
-    fetchData();
-  }, [productId, lineColor, label]);
-
-  useEffect(() => {
-    const scrollToleft = () => {
-      const chart = chartRef.current;
-      chart?.scrollIntoView();
-    };
-
-    let currentLabels: string[] = [];
-    let currentUserCount: number[] = [];
-
-    const interval = setInterval(async () => {
-      const result = await axios.get(`${URL}/post/posts/biz/${productId}`);
-      const { data } = result;
-
-      //새로운 날짜 추가
-      const newLabel = dayjs(data.slice(-1)[0].createdAt).format("MM-DD HH:mm");
-      const newCount = data.length;
-
-      currentLabels.push(newLabel);
-      currentUserCount.push(newCount);
-
-      // Limit the arrays to the same length to ensure 1:1 correspondence
-      const maxLength = 10; // Adjust this value as needed
-      if (currentLabels.length > maxLength) {
-        currentLabels.shift();
-      }
-      if (currentUserCount.length > maxLength) {
-        currentUserCount.shift();
-      }
-
-      setData((prevData) => {
-        return {
-          labels: currentLabels,
-          datasets: [
             {
-              ...prevData.datasets[0],
-              data: currentUserCount,
+                label,
+                yAxisID: "y-right",
+                data: [],
+                borderColor: lineColor,
+                backgroundColor: lineColor.split(")").join(", 0.5)"),
             },
-          ],
+        ],
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios.get(`${URL}/post/posts/biz/${productId}`);
+            const {data} = result;
+            console.log(data)
+
+
+            //x축 라벨
+            let labels = data.map((item: BizPriceType) =>
+                dayjs(item.createdAt).format("MM-DD HH:mm")
+            );
+
+            //y축 데이터
+            const userCount = Object.values(countDuplicates(labels));
+            labels = [...new Set(labels)]
+            console.log(labels, userCount)
+
+            setData({
+                labels,
+                datasets: [
+                    {
+                        label,
+                        yAxisID: "y-right",
+                        data: userCount,
+                        borderColor: lineColor,
+                        backgroundColor: lineColor.split(")").join(", 0.5)"),
+                    },
+                ],
+            });
         };
-      });
+        fetchData();
+    }, [productId, lineColor, label]);
 
-      scrollToleft();
-    }, 1500);
-    scrollToleft();
+    useEffect(() => {
+        const scrollToleft = () => {
+            const chart = chartRef.current;
+            chart?.scrollIntoView();
+        };
 
-    return () => clearInterval(interval);
-  }, [productId]);
-  return (
-    <div className="flex">
-      <Line options={options as any} data={data} width={600} height={600} />
-      <div ref={chartRef} />
-    </div>
-  );
+        let currentLabels: string[] = [];
+        let currentUserCount: number[] = [];
+
+        const interval = setInterval(async () => {
+            const result = await axios.get(`${URL}/post/posts/biz/${productId}`);
+            const {data} = result;
+
+            //x축 라벨
+            let labels = data.map((item: BizPriceType) =>
+                dayjs(item.createdAt).format("MM-DD HH:mm")
+            );
+
+            //y축 데이터
+            const userCount = Object.values(countDuplicates(labels));
+            labels = [...new Set(labels)]
+            console.log(labels, userCount)
+
+
+            setData((prevData) => {
+                return {
+                    labels,
+                    datasets: [
+                        {
+                            ...prevData.datasets[0],
+                            data: userCount
+                        },
+                    ],
+                };
+            });
+
+            scrollToleft();
+        }, 1500);
+        scrollToleft();
+
+        return () => clearInterval(interval);
+    }, [productId]);
+    return (
+        <div className="flex">
+            <Line options={options as any} data={data} width={600} height={600}/>
+            <div ref={chartRef}/>
+        </div>
+    );
 }
 
 export default UserCountChart;
